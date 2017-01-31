6b87af2305fb3f331ec018f2bd0ed173706eb1ad
Nilanjan Roy <ndeyroy@omniupdate.com>
2017-01-30 23:24:07 -0800

    Fixed an issue where the upload url was mistakenly removed

--- app/static/scripts/actions/uploadActions.js
+++ app/static/scripts/actions/uploadActions.js
@@ -6,6 +6,7 @@ export function uploadFile(data){
             var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
         }
     };
+    const request = axios.post('dev/upload/put', data, config);
     return {
         type: "CREATE_LNK",
         payload: request
